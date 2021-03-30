61da65f8f02d51cee3e1f223ca71e036a0401d90
Максим <kyngfy-panda@yandex.ru>
2021-03-31 02:12:59 +0300

    Оптимизировал рисование окружности Брезенхэма

--- GraphicsEditor/VisualTools/BresenhamCircleTool.cs
+++ GraphicsEditor/VisualTools/BresenhamCircleTool.cs
@@ -20,117 +20,39 @@
         {
             int radius = (int)Math.Min((int)MathGeometry.Length(StartPoint, currentPoint),
                 Math.Min(StartPoint.X, StartPoint.Y));
-            if (radius > 0) return;
-           // var points = GenerateCircleOctant(radius);
-            byte[] colorData = { color.B, color.G, color.R, color.A };
-          //   DrawCircle(CircleOctantReflection(), bitmap, colorData);
+            if (radius < 1) return;
+            BresenhamCircle(bitmap, color, StartPoint, radius);
         }
 
-        //public List<Point> GenerateCircleOctant(int radius)
-        //{
-        //    var points = new List<Point>[radius * 4];
-        //    int x = 0;
-        //    int y = radius;
-        //    int maxY = (int)Math.Round(y / Math.Sqrt(2));
-        //    int δ = (1 - radius) >> 1;
-        //    int count = 0;
-        //    while (y >= maxY)
-        //    {
-        //        points[count] = x;
-        //        points[count + 1] = y;
-        //        count += 2;
-
-        //        switch (δ)
-        //        {
-        //            case < 0:
-        //            {
-        //                x++;
-        //                if (((δ >> 1) + (y >> 1) - 1) <= 0)
-        //                {
-        //                    y--;
-        //                    δ += (x - y + 1) >> 1;
-        //                }
-        //                else
-        //                {
-        //                    δ += (x >> 1) + 1;
-        //                }
-
-        //                continue;
-        //            }
-
-        //            case > 0:
-        //            {
-        //                y--;
-        //                if (((δ >> 1) - (x >> 1) - 1) <= 0)
-        //                {
-        //                    δ -= (y >> 1) - 1;
-        //                }
-        //                else
-        //                {
-        //                    x++;
-        //                    δ += (x >> 1) + 1;
-        //                }
-
-        //                continue;
-        //            }
-
-        //            default:
-        //            {
-        //                x++;
-        //                y--;
-        //                δ += (x - y + 1) >> 1;
-        //                break;
-        //            }
-        //        }
-        //    }
-
-        //    return []
-        //}
-
-        //public int[] CircleOctantReflection(int[] points, int count)
-        //{
-        //    var circle = new int[count * 8];
-        //    for (int i = 0; i < count; i++)
-        //    {
-        //        circle[i] = points[i];
-        //    }
-
-        //    var reflections = new (int x1, int y1, int x2, int y2)[] { (0, 1, 1, 0), (-1, 0, 0, 1), (1, 0, 0, -1) };
-        //    foreach (var reflection in reflections)
-        //    {
-        //        for (int i = 0; i < count; i += 2)
-        //        {
-        //            circle[count + i] = (circle[i] * reflection.x1) + (circle[i + 1] * reflection.y1);
-        //            circle[count + i + 1] = (circle[i] * reflection.x2) + (circle[i + 1] * reflection.y2);
-        //        }
-
-        //        count <<= 1;
-        //    }
-
-        //    return circle;
-        //}
-
-        protected void DrawCircle(int[] points, WriteableBitmap bitmap, byte[] color)
+        private void BresenhamCircle(WriteableBitmap bitmap, Color color, Point center, int radius)
         {
-            int startX = (int)StartPoint.X;
-            int startY = (int)StartPoint.Y;
-
-            for (int i = 0; i < points.Length; i += 2)
+            int centerX = (int)center.X;
+            int centerY = (int)center.Y;
+            int x = radius;
+            int y = 0;
+            int radiusError = 1 - x;
+            while (x >= y)
             {
-                bitmap.WritePixels(new Int32Rect(startX + points[i], startY + points[i + 1], 1, 1), color, 4, 0);
-            }
-        }
+                WritePixel(bitmap, color, x + centerX, y + centerY);
+                WritePixel(bitmap, color, y + centerX, x + centerY);
+                WritePixel(bitmap, color, -x + centerX, y + centerY);
+                WritePixel(bitmap, color, -y + centerX, x + centerY);
+                WritePixel(bitmap, color, -x + centerX, -y + centerY);
+                WritePixel(bitmap, color, -y + centerX, -x + centerY);
+                WritePixel(bitmap, color, x + centerX, -y + centerY);
+                WritePixel(bitmap, color, y + centerX, -x + centerY);
 
-        protected override void Clear()
-        {
-            int radius = (int)Math.Min((int)MathGeometry.Length(StartPoint, LastPoint),
-                Math.Min(StartPoint.X, StartPoint.Y));
-            var size = new Int32Rect(
-                (int)StartPoint.X - radius,
-                (int)StartPoint.Y - radius,
-                (int)(LastPoint.X + radius),
-                (int)(LastPoint.Y + radius));
-           // Foreground.WritePixels(size, BackgroundBuffer, 4 * size.Width, 0);
+                y++;
+                if (radiusError < 0)
+                {
+                    radiusError += (y << 1) + 1;
+                }
+                else
+                {
+                    x--;
+                    radiusError += (y - x + 1) << 1;
+                }
+            }
         }
     }
 }
